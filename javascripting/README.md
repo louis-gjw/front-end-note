96dc80195092adae6a857ebcac8ccf395f0a5ff3
Apple <328664896@qq.com>
2017-09-15 17:19:18 +0800

    add pageroll

---
+++ javascripting/README.md
@@ -0,0 +1,3 @@
+javascripting : 查看任务
+javascripting run 你的文件路径 
+javascripting verify 你的文件路径 
---
+++ javascripting/function-arguments.js
@@ -0,0 +1,4 @@
+function math(firstNum,secondNum,thirdNum){
+  return firstNum + secondNum * thirdNum
+}
+console.log(math(53,61,67))
